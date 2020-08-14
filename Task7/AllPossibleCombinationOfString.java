3de833e1c3fdfad162fc18ef195dc75531639304
vigneshprabhup <68711174+vigneshprabhup@users.noreply.github.com>
2020-08-14 00:47:18 -0400

    possible combinations of String
    Find all possible combinations of String? (using recursion)

---
+++ Task7/AllPossibleCombinationOfString.java
@@ -0,0 +1,30 @@
+package Task7;
+
+public class AllPossibleCombinationOfString {
+	
+	static void combination(String str, String ans)
+	{
+		if(str.length()==0)
+		{
+			System.out.println(ans + " ");
+			return;
+	    }
+		
+		for(int i=0;i<str.length();i++)
+		{
+			char ch=str.charAt(i);
+			String ros= str.subSequence(0, i) + str.substring(i+1);
+			combination(ros,ans+ch);
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s ="abc";
+        
+        combination(s,"");
+		
+	}
+
+}
